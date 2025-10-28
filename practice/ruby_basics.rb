08e0c07a733a43f085bb64f57478e69a14206350
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-10-28 11:00:03 +0900

    ruby test

---
+++ practice/ruby_basics.rb
@@ -0,0 +1,17 @@
+name = "루비"
+
+num = 5
+
+puts "#{name}입니다."
+puts "자동으로 줄바꿈 해줍니다."
+
+puts "10 + 5 = #{10 + num}"
+
+puts "루비 뭔가 재밌는데"
+
+# 주석은 이렇게
+
+print "이름을 입력하세요:"
+name2 = gets.chomp
+
+puts "제 이름은 #{name2}입니다"
