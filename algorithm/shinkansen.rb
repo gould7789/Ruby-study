f9f976bfe1558da0e4809804f2fab1d4ee31c00d
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-11-07 09:59:05 +0900

    comment update

--- algorithm/shinkansen.rb
+++ algorithm/shinkansen.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 # JR 北日本は過去数年の分析に基づいて、新幹線の搭乗券の売れ行きを計算しています。
 # 今、7 日間で $250 の搭乗券を、15 万人が購入することがわかっています。
 # 統計を取ると、この価格帯から $10 高くなるたびに 7000 人が購入をやめ、
 # $10 安くなるたびに追加で 7000 人が搭乗券を購入することがわかりました。
-#（つまり、$260 では、14 万 3 千人が搭乗券を購入し、$270 では 13 万 6 千人が搭乗券を購入することになります。）
+# （つまり、$260 では、14 万 3 千人が搭乗券を購入し、$270 では 13 万 6 千人が搭乗券を購入することになります。）
 # 今、7 日間の搭乗券の価格 ticketPrice が与えられるので、
 # 搭乗券の購入人数を返す、weekly7DaysSales という関数を定義してください。
 
 # 함수 작성
-def weekly7DaysSales(ticketPrice)
+def weekly_7_days_sales(ticket_price)
   # $250를 기준으로 +- 기준값 계산
-  standard = (250 - ticketPrice) / 10.0
-  
+  standard = (250 - ticket_price) / 10.0
+
   # 150000에서 +- 할 합계
   total = 7000 * standard
   # 결과값
-  result = 150000 + total
+  result = 150_000 + total
 
-  return result.to_i
+  result.to_i
 end
 
-puts weekly7DaysSales(260)
-puts weekly7DaysSales(255)
-puts weekly7DaysSales(220)
+puts weekly_7_days_sales(260)
+puts weekly_7_days_sales(255)
+puts weekly_7_days_sales(220)
