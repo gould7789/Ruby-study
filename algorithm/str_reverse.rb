a2a12dc61f4fd7a21a44b6354aea55484a7e0e0b
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-11-12 15:38:40 +0900

    reverse string

---
+++ algorithm/str_reverse.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# 文字列 s が与えられるので、その文字列を反転する関数 reverseString を作成してください。
+
+def reverse_string(s)
+  # 문자열 뒤집기
+  s.reverse
+end
+
+puts reverse_string('a')
+puts reverse_string('John')
+puts reverse_string('Rerum voluptas')
