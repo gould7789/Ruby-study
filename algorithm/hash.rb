8b227305c709de572c5ebd8092bb51a4f141525d
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-11-19 12:38:50 +0900

    each_with_index and min_by method

--- algorithm/hash.rb
+++ algorithm/hash.rb
@@ -7,32 +7,37 @@
 # 配列が [1,5,3,4,3,1,6] の場合、最初に繰り返される要素は 1 なので 1 を返します。
 
 def first_repeating_number(nums)
-  # 빈도수를 기록할 해시를 생성
-  # {숫자 => 횟수} 형태로 저장
-  seen_numbers = {}
-  repeating_candidates = [] # 중복이 확인된 숫자들을 저장할 배열
+  # { 숫자 => 첫 등장 인덱스 } 를 기록
+  first_index = {}
 
-  # 배열을 순회
-  nums.each do |num|
-    if seen_numbers.key?(num)
-      # 중복이 발생하면, 그 숫자를 후보 배열에 추가
-      repeating_candidates << num
+  # 중복이 발생한 숫자들만 모아서 [숫자, 첫 등장 인덱스] 형태로 저장
+  duplicate_data = []
 
+  # 배열을 인덱스와 함께 순회
+  nums.each_with_index do |num, current_index|
+    # 이전에 등장한 적이 있다면 (중복이 확인되었다면)
+    if first_index.key?(num)
+      # 중복된 숫자와 그 숫자의 첫 등장 인덱스를 후보 목록에 추가
+      # 이 숫자의 첫 등장 인덱스는 first_index 해시에 기록되어 있음
+      duplicate_data << [num, first_index[num]]
     else
-      # 처음 등장하는 숫자는 기록
-      # 여기서는 밸류값이 중요하지 않기 때문에 불린값으로 저장 가능
-      seen_numbers[num] = true
+      # 처음 등장하는 숫자이므로, 첫 등장 인덱스를 기록
+      first_index[num] = current_index
     end
   end
 
-  # 중복이 하나도 없었다면 -1 반환
-  if repeating_candidates.empty?
-    -1
-  else
-    # 모든 중복 후보들 중에서 가장 작은 숫자를 반환
-    repeating_candidates.min
-  end
+  # 중복이 발생한 숫자가 없다면 -1 반환
+  return -1 if duplicate_data.empty?
+
+  # duplicate_data에서 '첫 등장 인덱스' (배열의 두 번째 요소: index)가
+  # 가장 작은 쌍을 찾아 그 숫자 (배열의 첫 번째 요소: num)를 반환
+  # min_by는 루비에서 특정 기준(블록)에 따라 최소값을 찾는 매우 유용한 메서드
+  result_pair = duplicate_data.min_by { |num, index| index }
+
+  # result_pair는 [숫자, 인덱스] 형태이므로, 숫자([0])만 반환
+  result_pair[0]
 end
 
 puts first_repeating_number([2, 12, 5, 10, 9, 8])
+puts first_repeating_number([1, 5, 3, 4, 3, 1, 6])
 puts first_repeating_number([11, 45, 32, 75, 88, 15, 15])
